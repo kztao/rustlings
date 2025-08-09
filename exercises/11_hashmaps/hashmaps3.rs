61ce58b54dcb5fc1cec0db31b05573f66cdb9a57
taokz <taokz@xctctech.com>
2025-08-09 18:10:53 +0800

    fix all

--- exercises/11_hashmaps/hashmaps3.rs
+++ exercises/11_hashmaps/hashmaps3.rs
@@ -31,6 +31,29 @@ fn build_scores_table(results: &str) -> HashMap<&str, TeamScores> {
         // Keep in mind that goals scored by team 1 will be the number of goals
         // conceded by team 2. Similarly, goals scored by team 2 will be the
         // number of goals conceded by team 1.
+        let team_score_1 = TeamScores{ 
+            goals_scored: team_1_score, 
+            goals_conceded: team_2_score 
+        };
+        let team_score_2 = TeamScores{ 
+            goals_scored: team_2_score, 
+            goals_conceded: team_1_score 
+        };
+        
+         // If the team already exists, we need to update its scores.
+        if let Some(team) = scores.get_mut(&team_1_name) {
+            team.goals_scored += team_1_score;
+            team.goals_conceded += team_2_score;
+        } else {
+            scores.insert(team_1_name, team_score_1);
+        }
+
+        if let Some(team) = scores.get_mut(&team_2_name) {
+            team.goals_scored += team_2_score;
+            team.goals_conceded += team_1_score;
+        } else {
+            scores.insert(team_2_name, team_score_2);
+        }
     }
 
     scores
